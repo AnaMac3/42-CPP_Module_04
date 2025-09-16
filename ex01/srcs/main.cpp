680ca63641ae97d26c5a2ce25bd1ee283812cbb9
Ana <163326614+AnaMac3@users.noreply.github.com>
2025-09-16 17:07:50 +0200

    16.09.25 17h

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 10:25:18 by root              #+#    #+#             */
-/*   Updated: 2025/09/16 16:43:34 by root             ###   ########.fr       */
+/*   Updated: 2025/09/16 17:06:51 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 
 int	main()
 {
-	//Create an array of Animal objects, where half are Dog objects and the other half are Cat objects
+	//Create array of Animal objects
+	std::cout << "======================================================" << std::endl;
 	std::cout << "Create an array of Animal objects: 5 Dogs and 5 Cats." << std::endl;
 	const Animal* pets[10];
 	
@@ -28,9 +29,7 @@ int	main()
 			pets[i] = new Cat();
 	}
 
-	std::cout << std::endl;
-
-	std::cout << "Check types:" << std::endl;
+	std::cout << "\nCheck types:" << std::endl;
 	for (int i = 0; i < 10; i++)
 	{
 		std::cout << "Animal num " << i << " is " 
@@ -38,7 +37,9 @@ int	main()
 	}
 
 	std::cout << std::endl;
-	
+
+	std::cout << "======================================================" << std::endl;
+
 	std::cout << "Use Brains:" << std::endl;
 	
 	pets[0]->getBrain().setIdea("I am a Dog.", 0);
@@ -56,8 +57,9 @@ int	main()
 				<< pets[0]->getBrain().getIdea(2) << std::endl;
 	
 	std::cout << std::endl;
+	std::cout << "======================================================" << std::endl;
 
-	std::cout << "Use Brains: Another example:" << std::endl;
+	std::cout << "Assign Brain to pets[5] and test deep copy: " << std::endl;
 	Brain brain1;
 	
 	brain1.setIdea("I have a brain.", 0);
@@ -67,16 +69,25 @@ int	main()
 	brain1.setIdea("Bye!!", 7);
 	
 	pets[5]->getBrain() = brain1;
-	
+
+	std::cout << "\npets[5] ideas after assignment: " << std::endl;
 	for(int i = 0; i < 8; i ++)
 	{
 		std::cout << "Idea num " << i << ": "
 					<< pets[5]->getBrain().getIdea(i) << std::endl;
 	}
 	
-	std::cout << std::endl;
+	std::cout << "\nModify brain1 and check that pets[5] did not change: " << std::endl;
 	
-	std::cout << "Check deep copy:" << std::endl;
+	brain1.setIdea("I changed my mind!", 0);
+	std::cout << "brain1 idea 0 now is: " << brain1.getIdea(0) << std::endl;
+	std::cout << "pets[5] idea 0 still: " << pets[5]->getBrain().getIdea(0) << std::endl;
+	
+	std::cout << std::endl;
+	std::cout << "======================================================" << std::endl;
+	
+	std::cout << "Check deep copy with stack objects:" << std::endl;
+	std::cout << "1. With assignment operator: \n" << std::endl;
 	Dog dog1;
 	Dog dog2;
 
@@ -95,12 +106,13 @@ int	main()
 	std::cout << "dog1 idea 0 still: "
 				<< dog1.getBrain().getIdea(0) << std::endl;
 
+	std::cout << "\n2. With copy constructor: \n" << std::endl;
 	Dog dog3(dog2);
 
-	std::cout << "dog3 idea 0 is (a copt of dog2): "
+	std::cout << "dog3 idea 0 is (a copy of dog2): "
 				<< dog3.getBrain().getIdea(0) << std::endl;
 	
-	dog3.getBrain().setIdea("I wanna be a buterfly...", 1);
+	dog3.getBrain().setIdea("I wanna be a butterfly...", 1);
 	
 	std::cout << "dog3 idea 1 is: "
 				<< dog3.getBrain().getIdea(1) << std::endl;
@@ -110,12 +122,37 @@ int	main()
 	
 				
 	std::cout << std::endl;
+	std::cout << "======================================================" << std::endl;
+
+	std::cout << "Check deep copy with heap objects:" << std::endl;
 	
+	Cat* heapCat = new Cat();
+	
+	heapCat->getBrain().setIdea("I am a Cat in the Heap.", 0);
+
+	Cat* secondCat = new Cat(*heapCat);
+
+	std::cout << "secondCat's idea after the copy: "
+				<< secondCat->getBrain().getIdea(0) << std::endl;
+	
+	secondCat->getBrain().setIdea("MIAAAAAAU", 0);
+	std::cout << "secondCat's idea after changing it: "
+				<< secondCat->getBrain().getIdea(0) << std::endl;
+	
+	std::cout << std::endl;
+	std::cout << "======================================================" << std::endl;	
 	std::cout << "Delete Animal objects: " << std::endl;
 	for (int i = 0; i < 10; i++)
 		delete(pets[i]);
 	
 	std::cout << std::endl;
+
+	std::cout << "Delete heap cats: " << std::endl;
+	delete (heapCat);
+	delete (secondCat);
+
+	std::cout << std::endl;
+	std::cout << "Automatic deletes: " << std::endl;
 		
 	return (0);
 }
